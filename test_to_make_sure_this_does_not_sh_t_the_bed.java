7bd7a46f2c7888658ddc454cf9a4900a60894618
Jeremy Krovitz <jkrovitz@macalester.edu>
2017-03-16 19:13:34 -0500

    Update test_to_make_sure_this_does_not_sh_t_the_bed.java

--- test_to_make_sure_this_does_not_sh_t_the_bed.java
+++ test_to_make_sure_this_does_not_sh_t_the_bed.java
@@ -7,6 +7,11 @@ public class test_to_make_sure_this_does_not_sh_t_the_bed {
         System.out.println("Eggarld exclaims, 'Jeremy, take a goddamn shower!!!!'");
         System.out.println("Valerie says 'Watch as I attempt a casual shower.'");
         System.out.println("Infinite Loop makes an annoyed face.");
+        System.out.println("Maria, aka 'The Angry Russian,' gets angry.");
+        System.out.println("Mia says, 'Oh Man.'");
+        System.out.println("Chloe says, 'Oy veh. Jeremy get your tuchas in the shower.'");
+        System.out.println("Kaisy says, 'Seriously Jeremy it's been like a week.'");
+        System.out.println("Jeremy replies, 'It's only been a couple of days. I'm just rocking that #HairGetsGreasyQuicklyLife.'");
     }
 
 }
