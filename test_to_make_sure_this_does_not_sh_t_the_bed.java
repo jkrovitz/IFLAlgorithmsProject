fb16a912f5db0602a10155abe029444ea661b576
Jeremy Krovitz <jkrovitz@macalester.edu>
2017-03-16 19:05:59 -0500

    Add files via upload

---
+++ test_to_make_sure_this_does_not_sh_t_the_bed.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jkrovitz on 3/16/17.
+ */
+public class test_to_make_sure_this_does_not_sh_t_the_bed {
+    public static void main (String args[]){
+        System.out.println("Jeremy says, 'Testing Testing. One, Two, Three. Eggarld, Eggarld, Eggarald. Dawwww'er, Dawwww'er, Dawwww'er.'");
+        System.out.println("Eggarld exclaims, 'Jeremy, take a goddamn shower!!!!'");
+        System.out.println("Valerie says 'Watch as I attempt a casual shower.'");
+        System.out.println("Infinite Loop makes an annoyed face.");
+    }
+
+}
