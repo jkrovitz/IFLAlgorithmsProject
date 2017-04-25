4ad1a1840d642f23d6ce5ea136006a9e97bfb103
Luke <lbrown10@macalester.edu>
2017-04-25 14:09:55 -0500

    Added vertical RGB sort capabilities on whole rows. Also added comments. Code rapidly in need of clean up.

--- AlphaCode.java
+++ AlphaCode.java
@@ -15,7 +15,11 @@ import java.util.Scanner;
 public class AlphaCode {
 
 
-    public static void main(String args[]){
+    /**
+     * This is the part that actually runs, but mostly it just calls the DoTheSort fxn.
+     * @param args
+     */
+    public static void main(String args[]) {
         AlphaCode ac = new AlphaCode();
         ac.userQuestions();
         ac.DoTheSort();
@@ -27,15 +31,27 @@ public class AlphaCode {
     private int sortAttribute1;
     private int sortAttribute2;
     private boolean sortBigToSmall;
+    private int sortDirection;
     static BufferedImage img = null;
 
-    private void loadImage(String imageName){
+    /**
+     * This cute fellow runs a quick line of code to load up the image.
+     * @param imageName
+     */
+    private void loadImage(String imageName) {
+        System.out.println("Loading image...");
         try {
             img = ImageIO.read(new File(imageName));
         } catch (IOException e) {
         }
+        System.out.println("Loaded!");
     }
 
+    /**
+     * For now this function saves an output file, in a precise location on my machine.
+     * Change the location and file name to whatever you want.
+     * @param pic
+     */
     public void paint(BufferedImage pic) {
         try {
             File outputfile = new File("C:\\Users\\Luke\\Pictures\\savedAndSorted.jpg");
@@ -44,13 +60,15 @@ public class AlphaCode {
         }
     }
 
+    /**
+     * Temporary Giant ball of Yuck function, which asks impertinent questions on what the
+     * user wants to happen. Mostly demands ints. Needs fed and watered regularly.
+     */
     private void userQuestions() {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter an image file path to pixel sort:");
         String imageName = reader.next(); // Scans the next token of the input.
-        System.out.println("Loading image...");
         loadImage(imageName);
-        System.out.println("Loaded!");
         System.out.println("Choose your sorting Criteria: ");
         //ask for by which value: RGB, hue, saturation, or brightness
         System.out.println("Enter 1 for an RGB value, 2 for hue, 3 for saturation, and 4 for brightness.");
@@ -63,6 +81,8 @@ public class AlphaCode {
         sortBigToSmall = reader.nextBoolean();
         System.out.println("enter 1 for entire line, 2 for a set size chunks, 3 for random size chunks, 4 for edge detection, 5 for value range");
         sortStyle = reader.nextInt();
+        System.out.println("enter 1 for horizontal pixel sorting, 2 for vertical sorts ");
+        sortDirection = reader.nextInt();
 
         if (sortStyle == 2) {
             System.out.println("How size of chunks?"); //check for what size
@@ -84,52 +104,93 @@ public class AlphaCode {
             sortAttribute2 = reader.nextInt();
         }
     }
-    private void DoTheSort(){
-        if (sortStyle==1){
+
+    /**
+     * This is the main sort, which chooses which sort function to use, calling that helper function
+     */
+    private void DoTheSort() {
+        if (sortStyle == 1) {
             int imgWidth = img.getWidth();
             int imgHeight = img.getHeight();
-            if(sortValueChosen>=5){
+            if (sortValueChosen >= 5) {
                 sortRGB(imgWidth, imgHeight);
-                }
             }
         }
+    }
 
-    private void sortRGB(int wide, int high){
-        int CurrentRowIndex = 0;
-        for (int i = 0; i < high; i++) {
-            CurrentRowIndex = wide * i;
-            int arr[] = img.getRGB(0,i,wide,1,null,0,wide);
-            Color[] ColorArr = new Color[wide];
-            for (int j = 0; j < arr.length; j++) {
-                ColorArr[j] = new Color(arr[j]);
+    private void sortRGB(int wide, int high) {
+        if (sortDirection == 1) {
+            for (int i = 0; i < high; i++) {
+                int arr[] = img.getRGB(0, i, wide, 1, null, 0, wide);
+                Color[] ColorArr = new Color[wide];
+                for (int j = 0; j < arr.length; j++) {
+                    ColorArr[j] = new Color(arr[j]);
+                }
+                if (sortValueChosen == 5) {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getRed() - o2.getRed();
+                        }
+                    });
+                } else if (sortValueChosen == 6) {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getBlue() - o2.getBlue();
+                        }
+                    });
+                } else {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getBlue() - o2.getBlue();
+                        }
+                    });
+                }
+                int[] sortedArr = new int[wide];
+                for (int k = 0; k < arr.length; k++) {
+                    sortedArr[k] = ColorArr[k].getRGB();
+                }
+                img.setRGB(0, i, wide, 1, sortedArr, 0, wide);
             }
-            if (sortValueChosen==5){
-                Arrays.sort(ColorArr, new Comparator<Color>() {
-                    @Override
-                    public int compare(Color o1, Color o2) {
-                        return o1.getRed()-o2.getRed();
-                    }
-                });
-            }else if (sortValueChosen==6){
-                Arrays.sort(ColorArr, new Comparator<Color>() {
-                    @Override
-                    public int compare(Color o1, Color o2) {
-                        return o1.getBlue()-o2.getBlue();
-                    }
-                });
-            }else{
-                Arrays.sort(ColorArr, new Comparator<Color>() {
-                    @Override
-                    public int compare(Color o1, Color o2) {
-                        return o1.getBlue()-o2.getBlue();
-                    }
-                });
+        } else {//Do the whole thing 1 column at a time
+            for (int i = 0; i < wide; i++) {//iterate over each column, building an array of pixel colors in ints
+                int[] arr = new int[high];
+                for (int h = 0; h < high; h++) {
+                    arr[h] = img.getRGB(i,h);
+                }//create a new array of Color objects, for each pixel
+                Color[] ColorArr = new Color[high];
+                for (int j = 0; j < ColorArr.length; j++) {
+                    ColorArr[j] = new Color(arr[j]);
+                }//Now we sort, by writing new comparators to choose between RBG.
+                if (sortValueChosen == 5) {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getRed() - o2.getRed();
+                        }
+                    });
+                } else if (sortValueChosen == 6) {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getBlue() - o2.getBlue();
+                        }
+                    });
+                } else {
+                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                        @Override
+                        public int compare(Color o1, Color o2) {
+                            return o1.getBlue() - o2.getBlue();
+                        }
+                    });
+                }//Now, put the sorted Colors back into their columns
+                for (int k = 0; k < arr.length; k++) {
+                    img.setRGB(i,k,ColorArr[k].getRGB());
+                }
+
             }
-            int[] sortedArr = new int[wide];
-            for (int k = 0; k < arr.length; k++) {
-                sortedArr[k] = ColorArr[k].getRGB();
-            }
-            img.setRGB(0,i,wide,1,sortedArr,0,wide);
         }
     }
 }
