af6c8241972f4c47fbbab75795ae2b9bde9130fb
Luke <lbrown10@macalester.edu>
2017-04-26 19:35:42 -0500

    Added sorting by HSB values.

--- AlphaCode.java
+++ AlphaCode.java
@@ -107,61 +107,86 @@ public class AlphaCode {
     }
 
     /**
-     * This is the main sort, which chooses which sort function to use, calling that helper function
+     * This is the main sort function, which chooses which sort function to use, calling that helper function
      */
     private void DoTheSort() {
         if (sortStyle == 1) {
             int imgWidth = img.getWidth();
             int imgHeight = img.getHeight();
             if (sortValueChosen >= 5) {
-                if (sortDirection == 1) sortRGBHoriz(imgWidth, imgHeight,0,0);
-                else sortRGBVert(imgWidth, imgHeight,0,0);
+                if (sortDirection == 1) sortHoriz(imgWidth, imgHeight,0,0);
+                else sortVert(imgWidth, imgHeight,0,0);
             }
         }else if (sortStyle==2) tileSort();
 
     }
 
     /**
-     * This does RGB sorting in a horizontal direction.
+     * This does sorting in a horizontal direction.
      *
      * @param wide
      * @param high
      */
-    private void sortRGBHoriz(int wide, int high, int TopLeftX, int TopLeftY) {
+    private void sortHoriz(int wide, int high, int TopLeftX, int TopLeftY) {
             for (int i = 0; i < high; i++) {
                 int[] arr = new int[wide];
-                for (int h = 0; h < wide; h++) {//Iteratate over each pixel in the column.
+                for (int h = 0; h < wide; h++) {//Iterate over each pixel in the column.
                     arr[h] = img.getRGB(h+TopLeftX,i+TopLeftY);
                 }
                 Color[] ColorArr = new Color[wide];
                 for (int j = 0; j < arr.length; j++) {
                     ColorArr[j] = new Color(arr[j]);
-                }
-                if (sortValueChosen == 5) {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
-                        @Override
-                        public int compare(Color o1, Color o2) {
-                            if(sortBigToSmall) return o1.getRed() - o2.getRed();
+                }Arrays.sort(ColorArr, new Comparator<Color>() {
+                    @Override
+                    public int compare(Color o1, Color o2) {
+                        if (sortValueChosen == 5) {
+                            if (sortBigToSmall) return o1.getRed() - o2.getRed();
                             else return o2.getRed() - o1.getRed();
-                        }
-                    });
-                } else if (sortValueChosen == 6) {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
-                        @Override
-                        public int compare(Color o1, Color o2) {
-                            if(sortBigToSmall) return o1.getGreen() - o2.getGreen();
+                        }else if (sortValueChosen == 6) {
+                            if (sortBigToSmall) return o1.getGreen() - o2.getGreen();
                             else return o2.getGreen() - o1.getGreen();
-                        }
-                    });
-                } else {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
-                        @Override
-                        public int compare(Color o1, Color o2) {
+                        }else if (sortValueChosen==7) {
                             if(sortBigToSmall) return o1.getBlue() - o2.getBlue();
                             else return o2.getBlue() - o1.getBlue();
+                        }else{//Now, we make this work with HSV values
+                            float[] o1hsb = new float[3];//Building mini-arrays with the values
+                            Color.RGBtoHSB(o1.getRed(),o1.getGreen(), o1.getBlue(), o1hsb);
+                            float[] o2hsb = new float[3];
+                            Color.RGBtoHSB(o2.getRed(),o2.getGreen(), o2.getBlue(), o2hsb);
+                            if (sortValueChosen==2){//Check Hue
+                                if (sortBigToSmall) {
+                                    float ans = (o1hsb[0]-o2hsb[0]);
+                                    if (ans<0) return -1;
+                                    else return 1;
+                                }else {
+                                    float ans = (o2hsb[0] - o1hsb[0]);
+                                    if (ans < 0) return -1;
+                                    else return 1;
+                                }
+                            }else if (sortValueChosen==3){//Check Saturation
+                                if (sortBigToSmall) {
+                                    float ans = (o1hsb[1]-o2hsb[1]);
+                                    if (ans<0) return -1;
+                                    else return 1;
+                                }else {
+                                    float ans = (o2hsb[1] - o1hsb[1]);
+                                    if (ans < 0) return -1;
+                                    else return 1;
+                                }
+                            }if (sortValueChosen==4){//Check Brightness
+                                if (sortBigToSmall) {
+                                    float ans = (o1hsb[2]-o2hsb[2]);
+                                    if (ans<0) return -1;
+                                    else return 1;
+                                }else {
+                                    float ans = (o2hsb[2] - o1hsb[2]);
+                                    if (ans < 0) return -1;
+                                    else return 1;
+                                }
+                            }return 0; //A catch if all us breaks down somehow
                         }
-                    });
-                }
+                    }
+                });
                 for (int k = 0; k < arr.length; k++) {
                     img.setRGB(k+TopLeftX,i+TopLeftY,ColorArr[k].getRGB());
                 }
@@ -169,45 +194,71 @@ public class AlphaCode {
         }
 
     /**
-     * This does RGB sorting in a vertical direction.
+     * This does sorting in a vertical direction.
      * @param wide
      * @param high
      */
-    private void sortRGBVert(int wide, int high, int TopLeftX, int TopLeftY) {
+    private void sortVert(int wide, int high, int TopLeftX, int TopLeftY) {
             for (int i = 0; i < wide; i++) {//iterate over each column, building an array of pixel colors in ints
                 int[] arr = new int[high];
-                for (int h = 0; h < high; h++) {//Iteratate over each pixel in the column.
+                for (int h = 0; h < high; h++) {//Iterate over each pixel in the column.
                     arr[h] = img.getRGB(i+TopLeftX,h+TopLeftY);
                 }//create a new array of Color objects, for each pixel
                 Color[] ColorArr = new Color[high];
                 for (int j = 0; j < ColorArr.length; j++) {
                     ColorArr[j] = new Color(arr[j]);
-                }//Now we sort, by writing new comparators to choose between RBG.
-                if (sortValueChosen == 5) {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
+                }//Now we sort, by writing new comparators to choose between sort values.
+                Arrays.sort(ColorArr, new Comparator<Color>() {
                         @Override
                         public int compare(Color o1, Color o2) {
-                            if(sortBigToSmall) return o1.getRed() - o2.getRed();
-                            else return o2.getRed() - o1.getRed();
+                            if (sortValueChosen == 5) {
+                                if (sortBigToSmall) return o1.getRed() - o2.getRed();
+                                else return o2.getRed() - o1.getRed();
+                            }else if (sortValueChosen == 6) {
+                                if (sortBigToSmall) return o1.getGreen() - o2.getGreen();
+                                else return o2.getGreen() - o1.getGreen();
+                            }else if (sortValueChosen == 7) {
+                                if (sortBigToSmall) return o1.getBlue() - o2.getBlue();
+                                else return o2.getBlue() - o1.getBlue();
+                            }else{//Now, we make this work with HSV values
+                                float[] o1hsb = new float[3];//Building mini-arrays with the values
+                                Color.RGBtoHSB(o1.getRed(),o1.getGreen(), o1.getBlue(), o1hsb);
+                                float[] o2hsb = new float[3];
+                                Color.RGBtoHSB(o2.getRed(),o2.getGreen(), o2.getBlue(), o2hsb);
+                                if (sortValueChosen==2){//Check Hue
+                                    if (sortBigToSmall) {
+                                        float ans = (o1hsb[0]-o2hsb[0]);
+                                        if (ans<0) return -1;
+                                        else return 1;
+                                    }else {
+                                        float ans = (o2hsb[0] - o1hsb[0]);
+                                        if (ans < 0) return -1;
+                                        else return 1;
+                                    }
+                                }else if (sortValueChosen==3){//Check Saturation
+                                    if (sortBigToSmall) {
+                                        float ans = (o1hsb[1]-o2hsb[1]);
+                                        if (ans<0) return -1;
+                                        else return 1;
+                                    }else {
+                                        float ans = (o2hsb[1] - o1hsb[1]);
+                                        if (ans < 0) return -1;
+                                        else return 1;
+                                    }
+                                }if (sortValueChosen==4){//Check Brightness
+                                    if (sortBigToSmall) {
+                                        float ans = (o1hsb[2]-o2hsb[2]);
+                                        if (ans<0) return -1;
+                                        else return 1;
+                                    }else {
+                                        float ans = (o2hsb[2] - o1hsb[2]);
+                                        if (ans < 0) return -1;
+                                        else return 1;
+                                    }
+                                }return 0; //A catch if all us breaks down somehow
+                            }
                         }
-                    });
-                } else if (sortValueChosen == 6) {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
-                        @Override
-                        public int compare(Color o1, Color o2) {
-                            if(sortBigToSmall) return o1.getGreen() - o2.getGreen();
-                            else return o2.getGreen() - o1.getGreen();
-                        }
-                    });
-                } else {
-                    Arrays.sort(ColorArr, new Comparator<Color>() {
-                        @Override
-                        public int compare(Color o1, Color o2) {
-                            if(sortBigToSmall) return o1.getBlue() - o2.getBlue();
-                            else return o2.getBlue() - o1.getBlue();
-                        }
-                    });
-                }//Now, put the sorted Colors back into their columns
+                    });//Now, put the sorted Colors back into their columns
                 for (int k = 0; k < arr.length; k++) {
                     img.setRGB(i+TopLeftX,k+TopLeftY,ColorArr[k].getRGB());
                 }
@@ -221,27 +272,26 @@ public class AlphaCode {
         //First, check that the user did not put too big of a chunk size (extra chunky).
         if (sortAttribute1>img.getWidth() || sortAttribute1>img.getHeight()) {
             if (sortValueChosen >= 5) {
-                if (sortDirection == 1) sortRGBHoriz(img.getWidth(), img.getHeight(), 0, 0);
-                else sortRGBVert(img.getWidth(), img.getHeight(), 0, 0);
+                if (sortDirection == 1) sortHoriz(img.getWidth(), img.getHeight(), 0, 0);
+                else sortVert(img.getWidth(), img.getHeight(), 0, 0);
             }//Now, we get down to the business of subdividing chunks of the image at a time to do.
         }else{
             if(sortValueChosen>=5){
                 for (int i = 0; i < img.getHeight()/sortAttribute1; i++) {//For each row of tiles
                     for (int j = 0; j < img.getWidth()/sortAttribute1; j++) {//For each tile in that row
-                        if (sortDirection == 1) sortRGBHoriz(sortAttribute1, sortAttribute1,j*sortAttribute1,i*sortAttribute1);
-                        else sortRGBVert(sortAttribute1, sortAttribute1,j*sortAttribute1,i*sortAttribute1);
-
+                        if (sortDirection == 1) sortHoriz(sortAttribute1, sortAttribute1,j*sortAttribute1,i*sortAttribute1);
+                        else sortVert(sortAttribute1, sortAttribute1,j*sortAttribute1,i*sortAttribute1);
                     }//Now, cover the left edge partial tiles
-                    if (sortDirection == 1) sortRGBHoriz(img.getWidth()%sortAttribute1, sortAttribute1, img.getWidth() - (img.getWidth()%sortAttribute1),i*sortAttribute1);
-                    else sortRGBVert(img.getWidth()%sortAttribute1, sortAttribute1, img.getWidth() - (img.getWidth()%sortAttribute1),i*sortAttribute1);
+                    if (sortDirection == 1) sortHoriz(img.getWidth()%sortAttribute1, sortAttribute1, img.getWidth() - (img.getWidth()%sortAttribute1),i*sortAttribute1);
+                    else sortVert(img.getWidth()%sortAttribute1, sortAttribute1, img.getWidth() - (img.getWidth()%sortAttribute1),i*sortAttribute1);
                 }//Now, cover the bottom edge partial tiles
                 for (int j = 0; j < img.getWidth()/sortAttribute1; j++) {
-                    if (sortDirection == 1) sortRGBHoriz(sortAttribute1, img.getHeight()%sortAttribute1,j*sortAttribute1,img.getHeight() - (img.getHeight()%sortAttribute1));
-                    else sortRGBVert(sortAttribute1, img.getHeight()%sortAttribute1,j*sortAttribute1,img.getHeight() - (img.getHeight()%sortAttribute1));
+                    if (sortDirection == 1) sortHoriz(sortAttribute1, img.getHeight()%sortAttribute1,j*sortAttribute1,img.getHeight() - (img.getHeight()%sortAttribute1));
+                    else sortVert(sortAttribute1, img.getHeight()%sortAttribute1,j*sortAttribute1,img.getHeight() - (img.getHeight()%sortAttribute1));
                 }//Finally, the complicated mess of a line or two that gives us the bottom right corner tile
-                if (sortDirection == 1) sortRGBHoriz(img.getWidth()%sortAttribute1, img.getHeight()%sortAttribute1,img.getWidth() - (img.getWidth()%sortAttribute1),img.getHeight() - (img.getHeight()%sortAttribute1));
-                else sortRGBVert(img.getWidth()%sortAttribute1, img.getHeight()%sortAttribute1,img.getWidth() - (img.getWidth()%sortAttribute1),img.getHeight() - (img.getHeight()%sortAttribute1));
+                if (sortDirection == 1) sortHoriz(img.getWidth()%sortAttribute1, img.getHeight()%sortAttribute1,img.getWidth() - (img.getWidth()%sortAttribute1),img.getHeight() - (img.getHeight()%sortAttribute1));
+                else sortVert(img.getWidth()%sortAttribute1, img.getHeight()%sortAttribute1,img.getWidth() - (img.getWidth()%sortAttribute1),img.getHeight() - (img.getHeight()%sortAttribute1));
             }
         }
     }
-    }
+}
